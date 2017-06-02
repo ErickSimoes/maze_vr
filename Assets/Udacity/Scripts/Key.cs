bcdc8b7eaf762ebfcd0eb3b712735b1df9b5acd3
Erick Simões <erick.simoes@live.com>
2017-06-02 00:36:47 -0300

    feat: Destroy key when clicked
    - Play sound
    - Rotate key

--- Assets/Udacity/Scripts/Key.cs
+++ Assets/Udacity/Scripts/Key.cs
@@ -3,26 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour {
-	public int velocityRotation = 80;
+	private int velocityRotation = 80;
 	public GameObject[] keysPositions;
+	private AudioSource audioSource;
+	private bool haveKey = false;
+	private bool destroyedKey = false;
 
 	//Create a reference to the KeyPoofPrefab and Door
 
 	private void Awake() {
 		int randomKeyPosition = Random.Range(0, keysPositions.Length);
 		transform.position = keysPositions[randomKeyPosition].transform.position;
+
+		audioSource = GetComponent<AudioSource>();
+		audioSource.playOnAwake = false;
 	}
 
 	void Update() {
 		transform.Rotate(Vector3.up * Time.deltaTime * velocityRotation);
+
+		if (haveKey && !destroyedKey) {
+			velocityRotation += 80;
+			transform.localScale += new Vector3(0, -1f, 0.5f);
+			transform.position += Vector3.up * 0.01f;
+
+			if (transform.localScale.y < 0) {
+				destroyedKey = true;
+				Destroy(gameObject);
+			}
+		}
 	}
 
 	public void OnKeyClicked() {
 		// Instatiate the KeyPoof Prefab where this key is located
 		// Make sure the poof animates vertically
 		// Call the Unlock() method on the Door
-		// Set the Key Collected Variable to true
-		// Destroy the key. Check the Unity documentation on how to use Destroy
+		// ok - Set the Key Collected Variable to true
+		// ok - Destroy the key. Check the Unity documentation on how to use Destroy
+		haveKey = true;
+		audioSource.Play();
 	}
 
 }
