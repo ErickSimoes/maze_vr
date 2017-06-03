5d2971d782d5e8d026078389a3a83cacd1aabe06
Erick Simões <erick.simoes@live.com>
2017-06-03 16:30:48 -0300

    feat: Open chest top when as cliked

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -1,15 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Coin : MonoBehaviour 
-{
-    //Create a reference to the CoinPoofPrefab
+public class Coin : MonoBehaviour {
+	//Create a reference to the CoinPoofPrefab
+	public GameObject doorChest;
+	private bool rotate = false;
 
-    public void OnCoinClicked() {
-        // Instantiate the CoinPoof Prefab where this coin is located
-        // Make sure the poof animates vertically
-        // Destroy this coin. Check the Unity documentation on how to use Destroy
-    }
+	private void Update() {
+		if (rotate) {
+			doorChest.transform.Rotate(30 * Time.deltaTime, 0.0f, 0.0f);
+
+			if (doorChest.transform.eulerAngles.x > 350) {
+				rotate = false;
+			}
+		}
+	}
+
+	public void OnCoinClicked() {
+		// Instantiate the CoinPoof Prefab where this coin is located
+		// Make sure the poof animates vertically
+		// Destroy this coin. Check the Unity documentation on how to use Destroy
+		rotate = true;
+	}
 
 }
