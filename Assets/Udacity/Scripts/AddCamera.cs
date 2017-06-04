bc755458545ad53f504762f72ae715b924b1141f
Erick Simões <erick.simoes@live.com>
2017-06-04 06:25:06 -0300

    feat: Add canvas on camera

---
+++ Assets/Udacity/Scripts/AddCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AddCamera : MonoBehaviour {
+
+	public Canvas canvas;
+
+	// Use this for initialization
+	void Start() {
+		canvas.renderMode = RenderMode.ScreenSpaceCamera;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (canvas.worldCamera == null) {
+			canvas.worldCamera = GameObject.Find("Main Camera Right").GetComponent<Camera>();
+		}
+	}
+}
