1dfe5232c529deb5e82d2bff57f78d74112bcb17
Erick Simões <erick.simoes@live.com>
2017-06-06 21:59:26 -0300

    feat: Coin code transferred to Chest

---
+++ Assets/Udacity/Scripts/Chest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Chest : MonoBehaviour {
+	public GameObject doorChest;
+	private bool rotate = false;
+
+	private void Update() {
+		if (rotate) {
+			doorChest.transform.Rotate(30 * Time.deltaTime, 0.0f, 0.0f);
+
+			if (doorChest.transform.eulerAngles.x > 350) {
+				rotate = false;
+			}
+		}
+	}
+
+	public void OnChestClicked() {
+		rotate = true;
+	}
+}
